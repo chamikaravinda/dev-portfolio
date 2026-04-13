eddf8702698d520a22f00755cf1cfa29b6384f39
Chamika Ravinda <chamikaravinda@gmail.com>
2026-04-14 00:50:23 +0530

    Add github actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/dev-portfolio',
+  assetPrefix: '/dev-portfolio',
   images: {
     unoptimized: true,
   },
